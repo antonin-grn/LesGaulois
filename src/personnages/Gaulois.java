82be7fc5a27bca8410c01e05bac1b88ddc7ddb99
antonin-grn <antonin.gerain@gmail.com>
2026-01-08 11:46:14 +0100

    TP1 fin de l’exercice 3

--- src/personnages/Gaulois.java
+++ src/personnages/Gaulois.java
@@ -14,7 +14,12 @@ public class Gaulois {
 	}
 	
 	public void parler(String texte) {
-		System.out.println("Le gaulois " + nom + " : " + texte + ".");
+		System.out.println("Le gaulois " + nom + " : " + texte);
+	}
+	
+	public void frapper(Romain romain) {
+		System.out.println(nom + " envoie un grand coup dans la mâchoire de " + romain.getNom());
+		romain.recevoirCoup(force/3);
 	}
 	
 	public static void main(String[] args) {
--- src/test_fonctionnel/TestGaulois.java
+++ src/test_fonctionnel/TestGaulois.java
@@ -1,6 +1,7 @@
 package test_fonctionnel;
 
 import personnages.Gaulois;
+import personnages.Romain;
 
 public class TestGaulois {
 	public static void main(String[] args) {
@@ -8,6 +9,19 @@ public class TestGaulois {
 		System.out.println(asterix.getNom());
 		
 		asterix.parler("SIX SEVEEEENNNN");
+		
+		Romain minus = new Romain("Minus", 6);
+		
+		minus.parler("Top 7, FavEEEEEEEEE");
+		
+		/*for(int i = 0; i < 2; i++) {
+			minus.recevoirCoup(3);
+		}*/
+		
+		for(int i = 0; i < 3; i++) {
+			asterix.frapper(minus);
+		}
+		
 	}
 	
 }
